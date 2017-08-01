bd19bcdda8b7da9f66b16b7f0116b1e30e0ec785
Pei <jzha471@aucklanduni.ac.nz>
2017-08-01 17:00:52 +1200

    15. Creating a component ( under the server folder )

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import{Component} from '@angular/core' ;
+@Component({
+  selector: 'app-server' ,
+  templateUrl : './server.component.html'
+})
+//so we can use it outside
+export class ServerComponent
+ {
+
+ }
+
+
+
