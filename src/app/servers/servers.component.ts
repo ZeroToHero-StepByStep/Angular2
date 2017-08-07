ea262462ca612d2c5d239713e651257296a5be42
Pei <jzha471@aucklanduni.ac.nz>
2017-08-08 07:08:12 +1200

    29. Two-way databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false ;
   serverCreation = 'No server was created' ;
-  serverName = '' ;
+  serverName = 'TestServerInitially' ;
   constructor() {
     setTimeout(() =>{
       this.allowNewServer = true ;
