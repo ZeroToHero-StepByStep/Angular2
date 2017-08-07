a7b0d858907402e976cbcb08fb357400cf9d5f04
Pei <jzha471@aucklanduni.ac.nz>
2017-08-07 14:41:01 +1200

    21. Fully understanding the component selector ( using selector in attribute formate )

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   template: '<app-server></app-server><app-server></app-server>',
-  // styleUrls: ['./servers.component.css']
-  styles: [`h3{ color:dodgerblue ;  }`]
+  styleUrls: ['./servers.component.css']
+  // styles: [`h3{ color:dodgerblue ;  }`]
 })
 export class ServersComponent implements OnInit {
 
