168a398ec7713e370b5d713e03c0b8a994e7aef4
Pei <jzha471@aucklanduni.ac.nz>
2017-08-08 22:44:10 +1200

    38. Getting the index when using ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -41,7 +41,7 @@ export class ServersComponent implements OnInit {
     {
       this.displayOrHideDetailsButton = 'Hide Details' ;
     }
-    this.log.push(this.log.length + 1 ) ;
+    this.log.push( new Date() ) ;
 
   }
 
